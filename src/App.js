e1eb593c7cebdce40c4191723238faa90efae696
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-11-20 17:56:29 -0700

    view all managers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import List from "./components/List";
 
+const employees = [
+  {
+    id: 1,
+    first_name: "Joan",
+    last_name: "Smith",
+    department: "sales",
+    role: "manager",
+    manager: true
+  },
+  {
+    id: 2,
+    first_name: "Steven",
+    last_name: "Magnet",
+    department: "production",
+    role: "manager",
+    manager: true
+  },
+  {
+    id: 3,
+    first_name: "Cheese",
+    last_name: "McGee",
+    department: "sales",
+    role: "public relations",
+    manager: false
+  },
+  {
+    id: 4,
+    first_name: "Billy",
+    last_name: "Green",
+    department: "production",
+    role: "inventory",
+    manager: false
+  },
+  {
+    id: 5,
+    first_name: "Tyler",
+    last_name: "Wiltonsnyder",
+    department: "sales",
+    role: "online presence",
+    manager: false
+  },
+  {
+    id: 6,
+    first_name: "Juice",
+    last_name: "Simpson",
+    department: "central office",
+    role: "manager",
+    manager: true
+  }
+];
+/*
+resources:
+activity 16
+rendering the list, and linking to a json file
+
+activity 21
+render different views on different pages, and add navigation
+
+to-do
+make json list and load to page
+set up views for managers and all employees
+add sort functionality by name, by department, by role
+styling?
+deploy
+
+
+*/
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <List employees={employees} />;
 }
 
 export default App;
